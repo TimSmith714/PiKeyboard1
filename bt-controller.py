a617324292b83e57ad24b96e258c0892a2748ce4
Tim Smith <timsmith714@gmail.com>
2019-03-16 14:15:01 +0000

    Back to drawing board - need files from broken link

--- bt-controller.py
+++ bt-controller.py
@@ -3,5 +3,12 @@
 #This project will allow this Pi to work as a Bluetooth keyboard
 #The code is from the Linux User article, now offline, found at 
 # https://web.archive.org/web/20130709222233/http://www.linuxuser.co.uk/tutorials/emulate-a-bluetooth-keyboard-with-the-raspberry-pi
+# Sadly this link is now defunct found on wayback machine but there's a zip
+#referenced in the text that is missing
 
+## Other Stuff
+# I used this link to get the bluetooth working
+# https://raspberrypi.stackexchange.com/questions/41776/failed-to-connect-to-sdp-server-on-ffffff000000-no-such-file-or-directory?newreg=b7183805b8c548d8b5ea9d389deb350e
+
+# Back to the drawing board
 
