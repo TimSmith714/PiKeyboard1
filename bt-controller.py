39827c1fc2eae89c40e6190e4cce7c176d6128f6
Tim Smith <timsmith714@gmail.com>
2019-03-16 12:50:18 +0000

    Initial Commit

---
+++ bt-controller.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python2.7
+
+#This project will allow this Pi to work as a Bluetooth keyboard
+#The code is from the Linux User article, now offline, found at 
+# https://web.archive.org/web/20130709222233/http://www.linuxuser.co.uk/tutorials/emulate-a-bluetooth-keyboard-with-the-raspberry-pi
+
+
